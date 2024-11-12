b5afcf2ee98e23cac7b1f978f77fe984d1609ba3
shy-hy <iam2002hy@gmail.com>
2024-11-12 15:49:19 +0900

    20241112

--- LandingPage.sql
+++ LandingPage.sql
@@ -2,11 +2,11 @@ DROP DATABASE `LandingPage`;
 CREATE DATABASE `LandingPage`;
 USE `LandingPage`;
 
-CREATE TABLE `ASK`(
-`ask_number`	BIGINT	PRIMARY KEY AUTO_INCREMENT,
-`ask_name`	VARCHAR(255)	NOT NULL,
-`ask_phonenumber` VARCHAR(255) NOT NULL,
-`ask_content` VARCHAR(255) NULL,
-`ask_privacy_policy`	BOOLEAN	NOT NULL,
-`ask_marketing_policy`	BOOLEAN	NOT NULL
+CREATE TABLE `asks`(
+	`ask_id`	BIGINT	PRIMARY KEY AUTO_INCREMENT,
+	`ask_name`	VARCHAR(255)	NOT NULL,
+	`ask_phone_number` VARCHAR(255) NOT NULL,
+	`ask_content` TEXT,
+	`ask_privacy_policy`	BOOLEAN	NOT NULL,
+	`ask_marketing_policy`	BOOLEAN	NOT NULL
 );
